36f998f9b2e4738180e1630fd28858730dbf4651
Peter Dimov <pdimov@gmail.com>
2019-12-21 17:55:39 +0200

    Update include guard

--- include/boost/exception/exception.hpp
+++ include/boost/exception/exception.hpp
@@ -3,8 +3,8 @@
 //Distributed under the Boost Software License, Version 1.0. (See accompanying
 //file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_UUID_274DA366004E11DCB1DDFE2E56D89593
-#define BOOST_UUID_274DA366004E11DCB1DDFE2E56D89593
+#ifndef BOOST_EXCEPTION_274DA366004E11DCB1DDFE2E56D89593
+#define BOOST_EXCEPTION_274DA366004E11DCB1DDFE2E56D89593
 
 #include <boost/config.hpp>
 
@@ -479,4 +479,4 @@ boost
 #pragma warning(pop)
 #endif
 
-#endif // #ifndef BOOST_UUID_274DA366004E11DCB1DDFE2E56D89593
+#endif // #ifndef BOOST_EXCEPTION_274DA366004E11DCB1DDFE2E56D89593
